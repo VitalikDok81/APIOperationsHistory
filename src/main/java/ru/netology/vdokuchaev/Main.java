f70f36d7ffaffb09d778680eaa31a7f9e238198e
VitalikDok81 <vitalikdok@gmail.com>
2024-02-18 19:50:39 +0300

    ДЗ2

--- src/main/java/ru/netology/vdokuchaev/Main.java
+++ src/main/java/ru/netology/vdokuchaev/Main.java
@@ -1,11 +1,30 @@
 package ru.netology.vdokuchaev;
-
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Виталий");
+        System.out.println("Виталий");
+        int NumberTrans;
+        boolean InOutTrans;
+        float SumTrans ;
+        String Naznach;
+        String NamePayer;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите номер транзакции:");
+        NumberTrans = scanner.nextInt();
+        System.out.println("Введите сумму транзакции:");
+        SumTrans = scanner.nextFloat();
+        System.out.println("Введите плательщика:");
+        NamePayer = scanner.next();
+        System.out.println("Введите назначение транзакции:");
+        Naznach = scanner.next();
+
+        System.out.println("Номер транзакции: "+NumberTrans+", плательщик: "+NamePayer+", сумма: "+SumTrans+", назначение платежа: "+Naznach);
+
+
     }
 }
